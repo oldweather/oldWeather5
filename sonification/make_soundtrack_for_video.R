083c64d89ed8956b66b0cefc03e60d399f2036ba
Philip Brohan <philip@brohan.org>
2016-01-05 17:34:54 +0000

    New soundtrack with nicer sounds, and typing for transcriptions.

--- sonification/make_soundtrack_for_video.R
+++ sonification/make_soundtrack_for_video.R
@@ -3,12 +3,27 @@
 library(oldWeather5)
 library(tuneR)
 
-# load some sounds
+# load some sounds - all sampled at 24,000 hz
+celesta<-list()
+for(i in 1:27) {
+  fn<-sprintf("sounds/ltw/celesta/c%03d.mp3",i)
+  celesta[[i]]<-readMP3(fn)
+}
+clav<-list()
+for(i in 1:27) {
+  fn<-sprintf("sounds/ltw/clav/c%03d.mp3",i)
+  clav[[i]]<-readMP3(fn)
+}
 beep<-readWave('sounds/beep-01a.wav')
+beep<-downsample(beep,24000)
 twang<-readWave('sounds/twang.wav')
+twang<-downsample(twang,24000)
 cymbal<-readWave('sounds/cymbal.wav')
+cymbal<-downsample(cymbal,24000)
 chime<-readWave('sounds/chime.wav')
+chime<-downsample(chime,24000)
 keystroke<-readWave('sounds/keystroke.wav')
+keystroke<-downsample(keystroke,24000)
 
 # Get the length of the video, and the re-layouts, from the png files.
 files<-list.files(path=sprintf("%s/images/oW5.working/",Sys.getenv('SCRATCH')),
@@ -26,45 +41,60 @@ layout.frames<-18 # No. of frames for a layout change.
 film.length<-length(files)/framerate  # in s.
 
 # Make a blank sound file covering the whole period
-s.length<-film.length*beep@samp.rate
 sound.track<-beep
-sound.track@left<-rep(0,s.length)
+sound.track@samp.rate<-24000
+sound.track@left<-rep(0,film.length*sound.track@samp.rate)
 
-# Put in a beep for every classification in the period covered
+# Put in a sound for every classification in the period covered
 classifications<-ReadClassifications('../data-exports/classifications.csv')
-#subjects<-ReadSubjects('../../data-exports/subjects.csv')
 classifications<-InterpolateTimestamps(classifications)
+classifications<-SetIsTranscription(classifications)
 
 w<-GetClassificationsByDate(classifications,begin,end)
 begin.n<-as.numeric(begin)
 end.n<-as.numeric(end)
 for(i in w) {
   for(n in seq_along(classifications$annotations[[i]])) {
-    c.time<-classifications$annotations[[i]][[n]]$timestamp/1000
+    ann<-classifications$annotations[[i]][[n]]
+    c.time<-ann$timestamp/1000
     if(c.time<begin.n || c.time>end.n) next
     s.start<-(c.time-begin.n)/framerate*sound.track@samp.rate
     # Adjust for the re-layouts
     ct<-which(layout.times<c.time)
     s.start<-s.start+length(ct)*(layout.frames/framerate)*sound.track@samp.rate
     # Different sounds for different sorts of data
-    sample<-keystroke
-    if(!is.null(classifications$annotations[[i]][[n]]$type)) {
-    if(classifications$annotations[[i]][[n]]$type=='weather') sample<-cymbal
-    if(classifications$annotations[[i]][[n]]$type=='sea-ice') sample<-cymbal
-    if(classifications$annotations[[i]][[n]]$type=='date') sample<-chime
-    if(classifications$annotations[[i]][[n]]$type=='location') sample<-chime
-    if(classifications$annotations[[i]][[n]]$type=='events') sample<-twang
-    if(classifications$annotations[[i]][[n]]$type=='mentions') sample<-twang
-    if(classifications$annotations[[i]][[n]]$type=='refueling') sample<-twang
-    if(classifications$annotations[[i]][[n]]$type=='animals') sample<-twang
-    }
-    s.end<-s.start+length(sample@left)-1
-    if(s.end>length(sound.track@left)) {
-        space<-length(sound.track@left)-s.start-1
-        sample@left<-sample@left[1:space]
+    if(classifications$meta$is_transcription[i]) {     
+      sample<-keystroke
+      if(is.null(ann$contents) || nchar(ann$contents)<1) next
+      for(k in 1:nchar(ann$contents)) {
         s.end<-s.start+length(sample@left)-1
-    }
-    sound.track@left[s.start:s.end]<-sound.track@left[s.start:s.end]+sample@left
+        if(s.end>length(sound.track@left)) break
+        sound.track@left[s.start:s.end]<-sound.track@left[s.start:s.end]+
+                                         sample@left
+        s.start<-s.start+jitter(0.1,amount=0.02)*sound.track@samp.rate
+      }
+    } else {  # Annotation
+        y<-1000
+        if(!is.null(ann$y)) y<-ann$y
+        pitch.class<-as.integer(27*y/2300)+1
+        pitch.class<-min(pitch.class,27)
+        sample<-celesta[[pitch.class]]
+        if(!is.null(ann$type)&& (ann$type=='date' ||
+                                 ann$type=='location' ||
+                                 ann$type=='events' ||
+                                 ann$type=='mentions' ||
+                                 ann$type=='refueling' ||
+                                 ann$type=='animals')) {
+          sample<-clav[[pitch.class]]
+        }
+        s.end<-s.start+length(sample@left)-1
+        if(s.end>length(sound.track@left)) {
+            space<-length(sound.track@left)-s.start-1
+            sample@left<-sample@left[1:space]
+            s.end<-s.start+length(sample@left)-1
+          }
+       sound.track@left[s.start:s.end]<-sound.track@left[s.start:s.end]+sample@left
+      }
   }
 }
 sound.track<-normalize(sound.track,unit='16')
